69320589fad78bc5ea2ebdc6e3e780cff51de167
Moncef-Lak <76704813+Moncef-Lak@users.noreply.github.com>
2022-11-15 14:57:51 +0100

    animation

--- src/Components/myPicture.js
+++ src/Components/myPicture.js
@@ -1,50 +1,63 @@
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import React, { useCallback, useRef, useEffect, useState } from "react";
-import errorImage from './../images/not-found-image.jpg';
+import errorImage from "./../images/not-found-image.jpg";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const MyPicture = ({ image_name, index }) => {
-    const [imgUrl, setImgUrl] = useState(null);
-    let picture = useRef(null);
-    
-    useEffect(()=>{
-        setTimeout(()=>{
-            ScrollTrigger.refresh();
-        },1000)
-    },[])
-    const imgAnimation = useCallback(() => {
-        gsap.from(picture, 1, {
-            y: 300, opacity: 0,scale:.5,skewX:'10deg',
-            scrollTrigger: {
-                trigger: picture,
-                scrub: 1.2,
-                start:'top bottom',
-                end:'-200px center',
-                          
-            }
-        })
-    }, [])
+  const [imgUrl, setImgUrl] = useState(null);
+  let picture = useRef(null);
 
-    useEffect(() => {
-        try {
-            if (require('../images/gallery-image/' + image_name)) {
-                setImgUrl(require('../images/gallery-image/' + image_name).default)
-            }
-        } catch (error) {
-            setImgUrl(errorImage)
+  useEffect(() => {
+    setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 2000);
+  }, []);
+  const imgAnimation = useCallback(() => {
+    gsap.from(picture, 1, {
+      y: 300,
+      opacity: 0,
+      scale: 0.5,
+      skewX: "10deg",
+      scrollTrigger: {
+        trigger: picture,
+        scrub: 1.2,
+        start: "top bottom",
+        end: "-200px center",
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    try {
+      if (require("../images/gallery-image/" + image_name)) {
+        setImgUrl(require("../images/gallery-image/" + image_name).default);
+      }
+    } catch (error) {
+      setImgUrl(errorImage);
+    }
+    imgAnimation();
+  }, [image_name, imgAnimation]);
+
+  return (
+    <div
+      ref={(e) => (picture = e)}
+      className={`img-anim img-box-left  ${
+        index % 2 > 0 ? false : "img-box-right"
+      }`}
+    >
+      <img
+        src={imgUrl}
+        alt="img"
+        style={
+          index % 2 > 0
+            ? { left: Math.random() * 40 + "%" }
+            : { left: -Math.random() * 40 + "%" }
         }
-        imgAnimation();
-        
-    }, [image_name, imgAnimation])
-
-
-    return (
-        <div ref={e => picture = e} className={`img-anim img-box-left  ${index % 2 > 0 ? false : 'img-box-right'}`} >
-            <img src={imgUrl} alt='img' style={index % 2 > 0 ? { left: Math.random() * 40 + '%' } : { left: -Math.random() * 40 + '%' }} />
-        </div>
-    );
-}
+      />
+    </div>
+  );
+};
 
 export default React.memo(MyPicture);
